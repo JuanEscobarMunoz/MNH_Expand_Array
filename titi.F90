1d27f21527b62a7af4ad094ad3c143f6819da077
Juan Escobar <escj@aero.obs-mip.fr>
2022-08-04 15:22:46 +0200

    Juan 04/08/2022:titi.F90, test/use !$mnh_do_concurrent/!$mnh_end_do

--- titi.F90
+++ titi.F90
@@ -21,7 +21,7 @@ Fct(D(iib-1:,ijb-1:,:))
  
  !$mnh_expand_array(ii1=iib:iie:ip , ij=i2jb:ije , ik=ikb:ike:iks) 
 
-    F(iib:iie:ip,i2jb:ije,k) =  D( iib:iie:ip , i2jb:ije , k ) + Fct(D(iib-1:,ijb-1:,:))
+    F(iib:iie:ip,i2jb:ije,k) =  D ( iib:iie:ip , i2jb:ije , k ) + Fct(D(iib-1:,ijb-1:,:))
     C( iib: iie :ip , i2jb: ije , k)
 
     D( i: , j,k )
@@ -29,3 +29,9 @@ Fct(D(iib-1:,ijb-1:,:))
  !$mnh_end_expand_array(ii1=iib:iie:ip,ij=i2jb:ije,ik=ikb:ike:iks)
 
 Fct(D(iib-1:,ijb-1:,:))
+
+  !$mnh_do_concurrent(ii1=iib:iie:ip , ij=i2jb:ije , ik=ikb:ike:iks)
+
+     A(ii1,ij,ik) = B(ii1-1,ij,ik )
+
+  !$mnh_end_do()
